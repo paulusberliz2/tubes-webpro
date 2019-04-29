0491e91ae914630546aea192a579ba0685522d4d
DandiBaroes <dandibaroes88@gmail.com>
2019-04-29 09:56:48 +0700

    Membuat fungsi untuk menampilkan page yang dituju

--- Tubes_Webprofix/application/controllers/Cbook.php
+++ Tubes_Webprofix/application/controllers/Cbook.php
@@ -16,5 +16,47 @@ class Cbook extends CI_Controller {
 		$this->load->view('page_index');
 		$this->load->view('page_footer');
     }
+
+    public function advance()
+	{
+		$this->load->view('page_header');
+		$this->load->view('page_search');
+		$this->load->view('page_footer');
+	}
+
+  	public function buku()
+	{
+		$this->load->view('page_header');
+		$this->load->view('page_addBook');
+		$this->load->view('page_footer');
+	}
+
+	public function help()
+	{
+		$this->load->view('page_header');
+		$this->load->view('page_help');
+		$this->load->view('page_footer');
+	}
+
+	public function sigPage()
+	{
+		$this->load->view('page_header');
+		$this->load->view('page_signup');
+		$this->load->view('page_footer');
+    }
+    
+    public function logPage()
+	{
+		$this->load->view('page_header');
+		$this->load->view('page_login');
+		$this->load->view('page_footer');
+	}
+
+	public function reportPage()
+	{
+		$this->load->view('page_header');
+		$this->load->view('page_report');
+		$this->load->view('page_footer');
+	}
     
 }
