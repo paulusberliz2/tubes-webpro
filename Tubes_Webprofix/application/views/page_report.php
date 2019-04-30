a9b121c216bedac1b7c2a62a603cff3c517c7e9c
paulusberliz2 <47132292+paulusberliz2@users.noreply.github.com>
2019-04-30 15:24:12 +0700

    menambahkan page report

---
+++ Tubes_Webprofix/application/views/page_report.php
@@ -0,0 +1,54 @@
+<div class="card-body" style="height: 500px; background-color: white">
+                <div style="max-width: 918px;padding: 20px;color: #333;">
+                    <h1>How can we help?</h1>
+                </div>
+                <div style="padding: 0 20px 20px;">
+                    <form action method="post" name="reportForm" style="font-family: "Lucida Grande","Trebuchet MS",Geneva,Helvetica,sans-serif;">
+                        <p>
+                            Please check our <a href="/help/">Help Pages</a> and <a href="/help/faq">Frequently Asked Questions</a> (FAQ)
+                             to see if your question is answered there. Thank you.
+                        </p>
+                        <div style="padding-bottom: 8px;">
+                            <div class="label">
+                                <label for="email">Your Email Address</label>
+                            </div>
+                            <div class="input">
+                                <input type="text" class="email" name="email" id="email" style="width: 475px;"> (We'll need this if you want a reply)
+                            </div>
+                        </div>
+                        <div style="padding-bottom: 8px;">
+                            <div style="padding: 10px 0 2px;">
+                                <label for="topic">Topic</label>
+                            </div>
+                            <div class="input">
+                                <select class="required" name="topic" id="topic">
+                                    <option value="">Select...</option>
+                                    <option value="Borrowing Books">Borrowing Help</option>
+                                    <option value="Developer/Code">Developer/Code Question</option>
+                                    <option value="Editing Problem">Editing Issue</option>
+                                    <option value="Login Trouble">Login Trouble</option>
+                                    <option value="Spam Report">Spam Report</option>
+                                    <option value="Waiting List Issue">Waiting List</option>
+                                    <option value="Other">Other Question</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div style="padding-bottom: 8px;">
+                            <div style="padding: 10px 0 2px;">
+                                <label for="question">Your Question</label>
+                            </div>
+                            <div class="input">
+                                <textarea id="questionbox" name="questionbox" class="required" cols="65" rows="1"></textarea>
+                            </div>
+                            <p style="font-size: .875em;line-height: 1.5em;color: #333;">If you encounter an error message, please include it. For questions about our books, please provide the title/author or Open Library ID.</p>
+                        </div>
+                        <div style="padding-bottom: 0">
+                            <div style="width: 90%;">
+                                <input type="submit" class="largest" value="Send" name="send" title="Send"> &nbsp; &nbsp; 
+                                <a href="#" style="color: #e5432a;">Cancel</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
